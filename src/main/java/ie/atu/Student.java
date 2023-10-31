bae508573abbac34a9aa1ec391e290913e6f4655
Tamer Zraiq <g00425053@atu.ie>
2023-10-31 16:43:35 +0000

    Added class members and constructor

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -1,4 +1,13 @@
 package ie.atu;
-
+import java.util.Scanner;
 public class Student {
+    private String name;
+    private String email;
+    private String course;
+
+    public Student(String name, String email, String course) {
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
 }
